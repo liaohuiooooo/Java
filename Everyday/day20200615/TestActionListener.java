76a2ca51a1c37804812fed5fbf940f093bf516a7
liaohui <liaohuio_o@163.com>
2020-06-15 09:23:18 +0800

    Upload File

---
+++ Everyday/day20200615/TestActionListener.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TestActionListener{
+	public static void main(String[] args){
+		
+		System.out.println("OK");
+	}
+}
